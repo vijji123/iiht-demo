fd6c3fca3c39ca77772369b49360e9f39f2e550e
vijji123 <99134375+vijji123@users.noreply.github.com>
2022-02-10 15:41:55 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package demo.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
